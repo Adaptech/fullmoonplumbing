27d32e66dc7a18a222cc526c215c350a8b3c8c8d
Robert on Asus 305 <rreppel@gmail.com>
2016-11-04 09:34:13 -0700

    Mandatory fields experiment.

--- src/plumberList/Code.gs.js
+++ src/plumberList/Code.gs.js
@@ -1,59 +1,99 @@
 function onEdit(e){
-  Logger.log(e);
+  //  Logger.log(e);
+  var rowIndex = e.range.getRowIndex();
   var columnIndex = e.range.getColumnIndex(); 
   var tryingToEditAggregateIdColumn = columnIndex === 1;
   if(tryingToEditAggregateIdColumn) {
     Logger.log("Attempt to edit aggregateId. Exiting.");
     return
   }
-
-  // Validate input:
-  var oldValue = e.oldValue;
-  if(typeof oldValue == "undefined") {
-    oldValue = null;
+  
+  var oldValue = null;
+  if(typeof e.oldValue != "undefined") {
+    oldValue = e.oldValue.toString();
   }
-  var newValue = e.value;
-  //TODO: Field validations.
+  var newValue = e.value;  
+  Logger.log("Old value: " + oldValue + " New value: " + newValue);
 
+  // No idea why this is needed, but when a cell is empty, the e.value passed to onEdit contains an object with "{oldValue=somethingorother". Whose bug is that?
+  Logger.log("Type of e.value: " + typeof e.value);
+  if(typeof e.value == "object") {
+    Logger.log("New value was blank.");
+    newValue = "";
+  }
+
+  // What sheet are we on? 
+  // (The name of the sheet is used as the name of the aggregate being updated, e.g. "Plumber" or "Customer")
   var sheet = e.source;
   var sourceSheet = sheet.getActiveSheet();
   var sourceSheetName = sourceSheet.getName(); 
   Logger.log("sourceSheetName: " + sourceSheetName);
   
-  // Determine which property has changed:
+  // What property is being changed? 
+  // (The column heading in the first row of the sheet is used as the property name, e.g. the "Lastname" in C1 on the "Plumber" sheet is the lastname of a plumber.)
   var propertyName = sourceSheet.getRange(1, columnIndex).getValue();
   Logger.log("propertyName: " + propertyName);
+  
+  // Validate mandatory fields:
+  var validationRule = sourceSheet.getRange(2, columnIndex).getValue();
+  var isMandatoryField = validationRule === "mandatory";
+  Logger.log("isMandatoryField: " + isMandatoryField);
+  var editedCell = sourceSheet.getRange(rowIndex, columnIndex);
+  if(isMandatoryField && !newValue) {
+      Logger.log("No event logged: Mandatory field '" + propertyName + "' was empty.");
+      editedCell.setNote(propertyName + " is a required field.");
+      editedCell.setBackgroundRGB(255,204,204);
+      return
+  } else {
+      editedCell.clearNote();
+      editedCell.setBackground("white");
+  }
     
   // Determine if the change needs logging:
-  var singleCellEdited = e.range.getWidth() === 1 && e.range.getHeight() === 1;
+  var singleCellEdited =  e.range.getWidth() === 1 && e.range.getHeight() === 1;
+  Logger.log("singleCellEdited: " + singleCellEdited);
   var propertyNameFoundInRow1 = propertyName !== "";
-  var rowIndex = e.range.getRowIndex();
+  Logger.log("propertyNameFoundInRow1: " + propertyNameFoundInRow1);
   var notEditingPropertyNameCell = rowIndex > 1;
+  Logger.log("notEditingPropertyNameCell: " + notEditingPropertyNameCell);
   var notEditingAggregateIdCell = columnIndex > 1;
+  Logger.log("notEditingAggregateIdCell: " + notEditingAggregateIdCell);
+  var notEditingValidationRulesCell = rowIndex > 2;
+  Logger.log(" notEditingValidationRulesCell:" + notEditingValidationRulesCell);
   var notTryingToEditEventLogSheet = sourceSheetName !== "Event Log";
-  Logger.log("singleCellEdited: " + singleCellEdited + " propertyNameFoundInRow1: " + propertyNameFoundInRow1 + " notEditingPropertyNameCell: " + notEditingPropertyNameCell + " notTryingToEditEventLogSheet: " + notTryingToEditEventLogSheet);
-  if(notTryingToEditEventLogSheet && singleCellEdited && propertyNameFoundInRow1  && notEditingPropertyNameCell) {
-    Logger.log("Logging event ...");
-    var aggregateType = sourceSheetName;
-    Logger.log("aggregateType: " + aggregateType);    
-    var aggregateIdCell = sourceSheet.getRange('A' + rowIndex);
-    
-    // Determine aggregateId and eventType:
-    var aggregateId = aggregateIdCell.getValue();
-    if(aggregateIdCell.getValue() === "") {
-      aggregateId = Utilities.getUuid();
-      aggregateIdCell.setValue(aggregateId);
-      var eventType = aggregateType + "Created";
-    } else {
-      var eventType = aggregateType + "Updated";
-    }
-    Logger.log("eventType: " + eventType);
-    //TODO: How can we detect deletion events?
+  Logger.log(" notTryingToEditEventLogSheet: " + notTryingToEditEventLogSheet);
+  
+  if(singleCellEdited 
+     && propertyNameFoundInRow1
+     && notEditingPropertyNameCell
+     && notEditingAggregateIdCell
+     && notEditingValidationRulesCell
+     && notTryingToEditEventLogSheet) {
+        Logger.log("Logging event ...");
+        var aggregateType = sourceSheetName;
+        Logger.log("aggregateType: " + aggregateType);    
+        var aggregateIdCell = sourceSheet.getRange('A' + rowIndex);
         
-    // Log event:
-    var eventId = Utilities.getUuid();
-    var eventLog = sheet.getSheetByName("Event Log");
-    eventLog.appendRow([new Date(), eventId, eventType, aggregateId, aggregateType, propertyName, oldValue, newValue]);      
-    Logger.log(eventType + " event logged.");
-  }
+        // Determine aggregateId and eventType:
+        var aggregateId = aggregateIdCell.getValue();
+        if(aggregateIdCell.getValue() === "") {
+          aggregateId = Utilities.getUuid();
+          aggregateIdCell.setValue(aggregateId);
+          var eventType = aggregateType + "Created";
+          
+          // TODO: Give visual indication of mandatory fields?
+          
+          
+        } else {
+          var eventType = aggregateType + "Updated";
+        } // endif
+        Logger.log("eventType: " + eventType);
+        //TODO: How can we detect deletion events?
+            
+        // Log event:
+        var eventId = Utilities.getUuid();
+        var eventLog = sheet.getSheetByName("Event Log");
+        eventLog.appendRow([new Date(), eventId, eventType, aggregateId, aggregateType, propertyName, oldValue, newValue]);      
+        Logger.log(eventType + " event logged.");
+  } //endif
 }
